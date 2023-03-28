5bb20494ad56a0dfe143a1279e30e58e70d23279
sathishkumarg5024 <sathishkumarg5024@gmail.com>
2023-03-28 21:11:04 +0530

    Add intro-to-node

---
+++ intro-to-node/index.js
@@ -0,0 +1,8 @@
+var superheroes = require('superheroes');
+const superVillains = require('supervillains');
+
+var mySuperHeroName= superheroes.random();
+console.log(mySuperHeroName);
+
+var mySuperVillainName= superVillains.random();
+console.log(mySuperVillainName);
